7179b5f88c4117e76c36e6add0c109d2d7287e56
Kevin Ritchey <kevin@zenpantech.com>
2023-06-07 14:17:16 -0400

    finished setup script and linted main

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ Purpose: Backup BestCase CLIENTS Directory
 import argparse
 import subprocess
 import os
-import datetime
 import logging
 import json
+import tempfile
+import datetime
+import boto3
+from botocore.exceptions import ClientError
 
 
 # --------------------------------------------------
@@ -29,7 +32,7 @@ def get_args():
         metavar="str",
         type=str,
         required=False,
-        default="C:\BestCase\CLIENTS",
+        default="C:\\BestCase\\CLIENTS",
     )
 
     parser.add_argument(
@@ -63,7 +66,7 @@ def get_args():
 
 
 # --------------------------------------------------
-def compress_dir_7z(directory_path, output_file=None, debug=False, log_file=None):
+def compress_dir_7z(directory_path, output_file=None):
     """Compresses a directory using 7zip.
 
     Args:
@@ -79,18 +82,15 @@ def compress_dir_7z(directory_path, output_file=None, debug=False, log_file=None
     """
 
     if not os.path.exists(directory_path):
-        logging.critical(f"Directory '{directory_path}' does not exist.")
+        logging.critical("Directory '%d' does not exist.", directory_path)
         return [False, None]
 
     if not os.path.isdir(directory_path):
-        logging.critical(f"Path '{directory_path}' is not a directory.")
+        logging.critical("Path '%d' is not a directory.", directory_path)
         return [False, None]
 
     # Set default output path to TEMP directory if not provided
     if output_file is None:
-        import tempfile
-        import datetime
-
         current_datetime = datetime.datetime.now()
         current_datetime_iso = current_datetime.isoformat()
         current_datetime_iso_modified = current_datetime_iso.replace(":", "_")
@@ -121,13 +121,13 @@ def compress_dir_7z(directory_path, output_file=None, debug=False, log_file=None
             check=True,
         )
         return [True, output_file]
-    except subprocess.CalledProcessError as e:
-        logging.critical(f"Compression failed: {e}")
+    except subprocess.CalledProcessError as called_error:
+        logging.critical("Compression failed: %d", called_error)
         return [False, None]
 
 
 # --------------------------------------------------
-def send_backup(output_file, s3_bucket, use_boto=False, debug=False, log_file=None):
+def send_backup(output_file, s3_bucket, use_boto=False):
     """Sends a backup file to an S3 bucket.
 
     Args:
@@ -142,36 +142,33 @@ def send_backup(output_file, s3_bucket, use_boto=False, debug=False, log_file=No
     """
 
     if not os.path.exists(output_file):
-        logging.error(f"File '{output_file}' does not exist.")
+        logging.error("File '%d' does not exist.", output_file)
         return False
 
     if not os.path.isfile(output_file):
-        logging.error(f"Path '{output_file}' is not a file.")
+        logging.error("Path '%d' is not a file.", output_file)
         return False
 
     if use_boto:
         try:
-            import boto3
-
-            s3 = boto3.resource("s3")
-            s3.meta.client.upload_file(output_file, s3_bucket, output_file)
+            s3_client = boto3.client("s3")
+            response = s3_client.upload_file(output_file, s3_bucket, output_file)
+            logging.info("Response: %d", response)
             return True
-        except Exception as e:
-            logging.critical(f"Sending backup via Boto3 failed: {e}")
+        except ClientError as s3_error:
+            logging.critical("Sending backup via Boto3 failed: %d", s3_error)
             return False
     else:
         try:
             subprocess.run(["aws", "s3", "cp", output_file, s3_bucket], check=True)
             return True
-        except subprocess.CalledProcessError as e:
-            logging.critical(f"Sending backup via AWS CLI failed: {e}")
+        except subprocess.CalledProcessError as called_error:
+            logging.critical("Sending backup via AWS CLI failed: %d", called_error)
             return False
 
 
 # --------------------------------------------------
-def send_msg_SNS(
-    message, recipient, use_boto=False, subject=None, debug=False, log_file=None
-):
+def send_msg_sns(message, recipient, use_boto=False, subject=None):
     """Sends a message to an SNS topic
 
     Uses the Boto3 library if available, else sends the message via AWS CLI.
@@ -193,13 +190,13 @@ def send_msg_SNS(
 
     if use_boto:
         try:
-            import boto3
-
             sns = boto3.client("sns")
             sns.publish(TopicArn=recipient, Message=message, Subject=subject)
             return True
-        except Exception as e:
-            logging.critical(f"Sending message via Boto3 to SNS topic failed: {e}")
+        except ClientError as client_error:
+            logging.critical(
+                "Sending message via Boto3 to SNS topic failed: %d", client_error
+            )
             return False
     else:
         try:
@@ -218,43 +215,15 @@ def send_msg_SNS(
                 check=True,
             )
             return True
-        except subprocess.CalledProcessError as e:
-            logging.critical(f"Sending message via AWS CLI to SNS topic failed: {e}")
+        except subprocess.CalledProcessError as called_error:
+            logging.critical(
+                "Sending message via AWS CLI to SNS topic failed: %d", called_error
+            )
             return False
 
 
 # --------------------------------------------------
-def test_boto3(debug=False, log_file=None):
-    """Tests if boto3 is installed"""
-
-    try:
-        # test if boto3 is installed
-        import boto3
-
-        return True
-    except ModuleNotFoundError:
-        logging.error("boto3 is not installed.")
-        return False
-
-
-# --------------------------------------------------
-def test_awsc(debug=False, log_file=None):
-    """Tests if aws cli is installed"""
-
-    try:
-        # test if aws is installed
-        subprocess.run(["aws"], stdout=subprocess.DEVNULL, check=True)
-        return True
-    except FileNotFoundError:
-        logging.error("aws is not installed.")
-        return False
-    except subprocess.CalledProcessError:
-        logging.error("aws is not installed.")
-        return False
-
-
-# --------------------------------------------------
-def prune_backups(s3_bucket, days=7, use_boto=False, debug=False, log_file=None):
+def prune_backups(s3_bucket, days=7, use_boto=False):
     """Prunes old backups from an S3 bucket.
 
     Args:
@@ -270,25 +239,23 @@ def prune_backups(s3_bucket, days=7, use_boto=False, debug=False, log_file=None)
 
     if use_boto:
         try:
-            import boto3
-
-            s3 = boto3.resource("s3")
-            bucket = s3.Bucket(s3_bucket)
+            s3_client = boto3.resource("s3")
+            bucket = s3_client.Bucket(s3_bucket)
             for obj in bucket.objects.all():
                 if obj.last_modified < datetime.datetime.now(
                     datetime.timezone.utc
                 ) - datetime.timedelta(days=days):
                     obj.delete()
             return True
-        except Exception as e:
-            logging.critical(f"Pruning via Boto3 failed: {e}")
+        except ClientError as client_error:
+            logging.critical("Pruning via Boto3 failed: %d", client_error)
             return False
     else:
         try:
             # create list of backups to delete
-            backups_to_keep = []
-            bucket_name = s3_bucket.replace("s3://", "")
-            command = f"aws s3 ls s3://{bucket_name} --recursive"
+            # backups_to_keep = []
+            # bucket_name = s3_bucket.replace("s3://", "")
+            command = f"aws s3 ls {s3_bucket} --recursive"
             output = subprocess.check_output(command, shell=True).decode("utf-8")
             objects = []
 
@@ -315,8 +282,8 @@ def prune_backups(s3_bucket, days=7, use_boto=False, debug=False, log_file=None)
                 check=True,
             )
             return True
-        except subprocess.CalledProcessError as e:
-            logging.critical(f"Pruning via AWS CLI failed: {e}")
+        except subprocess.CalledProcessError as called_error:
+            logging.critical("Pruning via AWS CLI failed: %d", called_error)
             return False
 
 
@@ -325,19 +292,8 @@ def main():
     """Do the heavy lifting of backing up the BestCase CLIENTS directory
     to an S3 bucket"""
 
-    main_start_time = datetime.datetime.now()
     args = get_args()
     config_file = args.config_file
-
-    use_boto = False
-    use_aws = False
-
-    if debug:
-        logging.debug(f'CLIENTS directory = "{directory_path}"')
-        logging.debug(f'CLIENTS directory = "{directory_path}"')
-        logging.debug(f'S3 backup bucket = "{s3_bucket}"')
-        logging.debug(f'Debug = "{debug}"')
-        logging.debug(f'Config file = "{config_file}"')
 
     # Use config file if provided, else use command line arguments
     if config_file is not None:
@@ -345,10 +301,10 @@ def main():
         directory_path = config["clients"]
         s3_bucket = config["s3"]
         debug = config["debug"]
+        use_boto3 = config["use_boto3"]
     else:
-        directory_path = args.clients
-        s3_bucket = args.s3
-        debug = args.debug
+        logging.critical("No config file provided, exiting.")
+        return False
 
     # Exit if S3 bucket is 's3://my-bestcase-backup'
     if s3_bucket == "s3://my-bestcase-backup":
@@ -357,67 +313,53 @@ def main():
         return False
 
     # Define location of log file
-    if log_file == "None":
-        import tempfile
+    log_file = tempfile.gettempdir() + "\\BestCaseBackup"
 
-        temp_dir = tempfile.gettempdir()
-        log_file = temp_dir + "\\BestCaseBackup.log"
-
-    logging.basicConfig(filename=log_file, encoding="utf-8", level=logging.DEBUG)
+    # Set logging level and start logging
     if debug:
         logging.basicConfig(
-            format="%(asctime)s %(levelname)s: %(message)s", level=logging.DEBUG
+            filename=log_file,
+            encoding="utf-8",
+            format="%(asctime)s %(levelname)s: %(message)s",
+            level=logging.DEBUG,
         )
     else:
         logging.basicConfig(
-            format="%(asctime)s %(levelname)s: %(message)s", level=logging.INFO
+            filename=log_file,
+            encoding="utf-8",
+            format="%(asctime)s %(levelname)s: %(message)s",
+            level=logging.INFO,
         )
-    log_start_time = datetime.datetime.now()
-    logging.info(f"Log started at {log_start_time}")
-
-    # if we are using boto3, test if boto3 is installed otherwise
-    # test if aws cli is installed - Exit if neither is installed
-    use_boto = test_boto3(debug, log_file)
-    if use_boto == False:
-        use_aws = test_awsc(debug, log_file)
-        if use_aws == False:
-            logging.error("No AWS CLI or boto3 found, exiting.")
-            return False
+    logging.info("Log started at %d", datetime.datetime.now())
 
     # Compress the BestCase CLIENTS directory
-    [compress_success, output_file] = compress_dir_7z(
-        directory_path, output_file=None, debug=debug, log_file=log_file
-    )
+    [compress_success, output_file] = compress_dir_7z(directory_path, output_file=None)
 
     if not compress_success:
         logging.error("Compression failed, exiting now.")
         return False
-    else:
-        logging.info(f"Compressed file: {output_file}")
-        try:
-            copy_success = send_backup(
-                output_file, s3_bucket, use_boto, debug, log_file
-            )
-            if copy_success:
-                logging.info("Backup sent successfully.")
-                return True
-            else:
-                logging.warning("Sending backup failed w/o an exception code.")
-                return False
-        except Exception as e:
-            logging.critical(f"Sending backup failed: {e}")
+
+    logging.info("Compressed file: %d", output_file)
+
+    # Send the backup to the S3 bucket
+    try:
+        copy_success = send_backup(output_file, s3_bucket, use_boto3)
+        if not copy_success:
+            logging.warning("Sending backup failed w/o an exception code.")
             return False
-        finally:
-            os.remove(output_file)
-            main_end_time = datetime.datetime.now()
-            elapsed_time = main_end_time - main_start_time
-            logging.info(f"Elapsed time: {elapsed_time}")
+        logging.info("Backup sent successfully.")
+        return True
+    except ClientError as send_error:
+        logging.critical("Sending backup failed: %d", send_error)
+        return False
+    finally:
+        os.remove(output_file)
 
 
 # --------------------------------------------------
 if __name__ == "__main__":
     start_time = datetime.datetime.now()
-    success = main()
+    main()
     end_time = datetime.datetime.now()
     elapsed_time = end_time - start_time
-    logging.info(f"Elapsed time: {elapsed_time}") if success else None
+    logging.info("Elapsed time: %d", elapsed_time)
--- setup.py
+++ setup.py
@@ -8,75 +8,223 @@ Purpose: creates a config file
 import json
 import os
 import sys
+import subprocess
 import logging
 
 
 # --------------------------------------------------
-def test_whether_boto3_is_installed():
+def test_whether_boto3_is_installed(doc_site):
     """Test whether boto3 is installed"""
     try:
         import boto3
     except ImportError:
         logging.warning("ERROR: boto3 is not installed")
+        logging.warning("Please see %d for installation instructions", doc_site)
+
         return False
-    
+
     return True
 
 
 # --------------------------------------------------
-def test_aws_cli_is_installed():
-    """Test whether AWS CLI is installed"""
+def install_boto3():
+    """Install boto3"""
     try:
-        import awscli
-    except ImportError:
-        logging.warning("ERROR: AWS CLI is not installed")
+        subprocess.run(
+            ["pip", "install", "boto3"],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+            check=True,
+        )
+    except subprocess.CalledProcessError as called_error:
+        logging.warning("ERROR: boto3 could not be installed")
+        logging.warning("ERROR: %d", called_error)
+        logging.warning("Please install boto3 manually")
         return False
-    
+
+    return True  # boto3 was installed
+
+
+# --------------------------------------------------
+def test_aws_cli_is_installed():
+    """Test whether AWS CLI executable is installed and in my PATH"""
+    try:
+        subprocess.run(
+            ["aws", "--version"],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+            check=True,
+        )
+    except subprocess.CalledProcessError as called_error:
+        logging.warning("ERROR: AWS CLI is not installed")
+        logging.warning("ERROR: %d", called_error)
+        return False
+
     return True
+
+
+# --------------------------------------------------
+def test_aws_cli_is_configured(doc_site):
+    """Test whether AWS CLI is configured"""
+    try:
+        # call subprocess but don't capture output
+        subprocess.run(
+            ["aws", "s3", "ls"],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+            check=True,
+        )
+    except subprocess.CalledProcessError as called_error:
+        logging.warning("ERROR: AWS CLI is not configured")
+        logging.warning("ERROR: %d", called_error)
+        logging.warning("Please see %d for configuration instructions", doc_site)
+        return False
+
+    return True
+
 
 # --------------------------------------------------
 def test_best_case_dir(best_case_dir):
     """Test if the Best Case directory exists"""
     if not os.path.isdir(best_case_dir):
-        logging.error(f"ERROR: '{best_case_dir}' is not a directory")
-        sys.exit(1)
+        logging.critical("ERROR: '%d' is not a directory", best_case_dir)
+        logging.warning("Please check your Best Case directory and try again")
+        return False
     clients_dir = os.path.join(best_case_dir, "CLIENTS")
     if not os.path.isdir(clients_dir):
-        logging.error(f"ERROR: '{clients_dir}' is not a directory")
-        sys.exit(1)
+        logging.critical("ERROR: '%d' is not a directory", clients_dir)
+        logging.warning("Please check your Best Case directory and try again")
+        return False
+    return True
 
 
 # --------------------------------------------------
-def create_config_file():
+def new_s3_bucket(s3_bucket):
+    """Prompt user to create a new S3 bucket"""
+    try:
+        subprocess.run(
+            ["aws", "s3", "mb", s3_bucket],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+            check=True,
+        )
+    except subprocess.CalledProcessError as called_error:
+        logging.warning("ERROR: AWS S3 bucket '%d' could not be created", s3_bucket)
+        logging.warning("ERROR: %d", called_error)
+        logging.warning("Please choose a different name for your S3 bucket")
+        return False
+
+    return True
+
+
+# --------------------------------------------------
+def test_s3_bucket(s3_bucket):
+    """Test if the S3 bucket exists or create S3 bucket"""
+    try:
+        # call subprocess but don't capture output
+        subprocess.run(
+            ["aws", "s3", "ls", s3_bucket],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+            check=True,
+        )
+    except subprocess.CalledProcessError as called_error:
+        logging.warning("ERROR: AWS S3 bucket '%d' does not exist", s3_bucket)
+        logging.warning("ERROR: %d", called_error)
+        logging.warning("Please create the S3 bucket or specify a different S3 bucket")
+        return False
+
+    return True
+
+
+# --------------------------------------------------
+def create_config_file(use_boto3):
+    """Create the config file"""
     config = {}
 
     # Prompt for Best Case installation directory
-    best_case_dir = input("Where is Best Case installed? E.g. C:\\BestCase: ")
-    test_best_case_dir(best_case_dir)
+    best_case_dir_exists = False
+    while not best_case_dir_exists:
+        best_case_dir = input("Where is Best Case installed? E.g. C:\\BestCase: ")
+        best_case_dir_exists = test_best_case_dir(best_case_dir)
     config["best_case_dir"] = best_case_dir
 
     # Prompt for AWS S3 bucket for backups
-    s3_bucket = input("What AWS S3 bucket will you use to store your backups? E.g. s3://my-bestcase-backups: ")
+    test_s3_bucket_exists = False
+    create_s3_bucket = False
+
+    while not create_s3_bucket:
+        do_create_s3_bucket = input(
+            "Do you want to create an AWS S3 bucket for backups? (Y/N): "
+        )
+        if do_create_s3_bucket.lower() == "y":
+            s3_bucket = input(
+                "What do you want to call the AWS S3 bucket? E.g. my-bestcase-backups: "
+            )
+            create_s3_bucket = new_s3_bucket(s3_bucket)
+        else:
+            while not test_s3_bucket_exists:
+                s3_bucket = input(
+                    "What S3 bucket will you use to store your backups? E.g. s3://my-bc-backups: "
+                )
+                test_s3_bucket_exists = test_s3_bucket(s3_bucket)
+
     config["s3_bucket"] = s3_bucket
 
     # Prompt for DEBUG mode
     debug_mode = input("Do you want to run the script in DEBUG mode? (Y/N): ")
     config["debug_mode"] = debug_mode.lower() == "y"
 
+    # Save use_bot3 to config file
+    config["use_boto3"] = use_boto3
+
     # Save configuration to JSON file
-    with open("config.json", "w") as file:
+    with open("config.json", "w", encoding="utf8") as file:
         json.dump(config, file, indent=4)
 
-    print("Config file created successfully.")
+    logging.info("Config file created successfully.")
 
 
 # --------------------------------------------------
 def main():
     """Make a jazz noise here"""
+    doc_site = (
+        "https://boto3.amazonaws.com/v1/documentation/api/" +
+        "latest/guide/quickstart.html#configuration"
+    )
+    windows_doc_site = (
+        "https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-windows.html"
+    )
+    logging.basicConfig(format="%(levelname)s - %(message)s", level=logging.INFO)
+
+    if not test_whether_boto3_is_installed(doc_site):
+        logging.info("INFO: boto3 is not installed")
+        input("Press Enter to install boto3")
+        if not install_boto3():
+            logging.error("ERROR: boto3 could not be installed")
+            logging.error("ERROR: Please see %d for installation instructions", doc_site)
+            input("Do you want to continue without installing boto3? (Y/N): ")
+            if input.lower() == "y":
+                use_boto3 = False
+            else:
+                logging.critical("ERROR: Will not continue without boto3")
+                sys.exit(1)
+        else:
+            use_boto3 = True
+    if not test_aws_cli_is_installed():
+        logging.error("ERROR: AWS CLI is not installed and boto3 is not installed")
+        logging.error(
+            "ERROR: Please install AWS CLI. See %d for instructions.", windows_doc_site
+        )
+        sys.exit(1)
+    if not test_aws_cli_is_configured(doc_site):
+        logging.error("ERROR: AWS CLI is not configured")
+        logging.error("ERROR: Please run 'aws configure' to configure AWS CLI")
+        sys.exit(1)
     # Create the config file
-    create_config_file()
+    create_config_file(use_boto3)
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
